bf471c1dbb7698efb28ddc376fb11f9ea497ea1b
Daniela <roehrig.87.daniela@gmx.de>
2025-03-13 13:14:41 +0100

    finish hw_6

--- hw_nr_6/src/components/UserProfile/UserProfile.jsx
+++ hw_nr_6/src/components/UserProfile/UserProfile.jsx
@@ -7,26 +7,26 @@ const UserProfile = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  
+  // Функция для загрузки данных пользователя
   const fetchUser = async () => {
-    setLoading(true); e
-    setError(null); 
+    setLoading(true); // Устанавливаем состояние загрузки в true
+    setError(null); // Сбрасываем ошибки перед новой загрузкой
     try {
       const { data } = await axios.get('https://randomuser.me/api/');
-      setUser(data.results[0]); 
+      setUser(data.results[0]); // Обновляем состояние с новыми данными пользователя
     } catch {
-      setError('Error while loading'); 
+      setError('Error while loading'); // Если ошибка, выводим сообщение
     } finally {
-      setLoading(false); 
+      setLoading(false); // Устанавливаем состояние загрузки в false
     }
   };
 
-  
+  // Вызовем fetchUser при монтировании компонента
   useEffect(() => {
     fetchUser();
   }, []);
 
-  
+  // Обработчик для кнопки, который вызывает fetchUser
   const handleFetchNewUser = () => {
     fetchUser();
   };
