2a9feee0859d52f26ce53a88310f99602447d6f5
Daniela <roehrig.87.daniela@gmx.de>
2025-03-19 18:13:26 +0100

    finish hw_nr_8

---
+++ hw_nr_8/src/components/App/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';  
+
+import './App.css';
+import ListItems from '../ListItems/ListItems.jsx';  
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Список элементов</h1>
+      <ListItems />
+    </div>
+  );
+}
+
+export default App;
